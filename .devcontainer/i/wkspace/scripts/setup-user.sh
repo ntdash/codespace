21b502bf86ba96f09cd7ae3af9db0bef65bfc27a
K. Matthaus EHON <ntdash@outlook.com>
2022-01-25 08:40:22 +0000

    feat (i/wkspace): ** change non-root default shell into zsh

--- .devcontainer/i/wkspace/scripts/setup-user.sh
+++ .devcontainer/i/wkspace/scripts/setup-user.sh
@@ -14,3 +14,18 @@ chmod 0440 /etc/sudoers.d/${USERNAME}
 
 # add basics groups to it
 usermod -aG optical,docker,storage "${USERNAME}"
+
+
+# install zsh and change the newly created user's default shell
+
+# install zsh
+pacman -S --noconfirm zsh
+
+# install ohmyzsh
+su ${USERNAME} -c 'bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+
+# enable docker and docker-compose plugin in zshrc
+sed -i -r 's/^plugins=\((.*)\)$/plugins=\(\1 docker docker-compose\)/' /home/${USERNAME}/.zshrc
+
+# change user default shell into zsh
+usermod -s /usr/bin/zsh "${USERNAME}"
