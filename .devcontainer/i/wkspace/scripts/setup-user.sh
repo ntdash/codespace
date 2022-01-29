37af51d22a8324e7a8abaec37459ad4053cda339
K. Matthaus EHON <ntdash@outlook.com>
2022-01-29 08:40:02 +0000

    ref: ** format comments

--- .devcontainer/i/wkspace/scripts/setup-user.sh
+++ .devcontainer/i/wkspace/scripts/setup-user.sh
@@ -5,27 +5,27 @@ UPASSWD=${2:-'secret'}
 
 echo -e "\nCreating a new user with root permission with the following credentials:\nusername: $USERNAME\npassword: $UPASSWD...\n\n"
 
-# create non-root user
+# Create non-root user
 useradd -mp "${UPASSWD}" "${USERNAME}"
 
-# grant root privilege to the newly created non-root user
+# Grant root privilege to the newly created non-root user
 echo ${USERNAME} ALL=\(ALL\) ALL > /etc/sudoers.d/${USERNAME}
 chmod 0440 /etc/sudoers.d/${USERNAME}
 
-# add basics groups to it
+# Add basics groups to it
 usermod -aG optical,docker,storage "${USERNAME}"
 
 
-# install zsh and change the newly created user's default shell
+# Install zsh and change the newly created user's default shell
 
-# install zsh
+# Install zsh
 pacman -S --noconfirm zsh
 
-# install ohmyzsh
+# Install ohmyzsh
 su ${USERNAME} -c 'bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
 
-# enable docker and docker-compose plugin in zshrc
+# Enable docker and docker-compose plugin in zshrc
 sed -i -r 's/^plugins=\((.*)\)$/plugins=\(\1 docker docker-compose\)/' /home/${USERNAME}/.zshrc
 
-# change user default shell into zsh
+# Change user default shell into zsh
 usermod -s /usr/bin/zsh "${USERNAME}"
